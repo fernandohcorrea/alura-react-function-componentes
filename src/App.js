ab825406a041f9b35c546792d89a78248a69177b
Fernando H Correa <fernando.correa@kymo.it>
2021-06-22 10:28:15 -0300

    20210622 -useState Hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { Container, Typography } from '@material-ui/core';
+import 'fontsource-roboto';
 import './App.css';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
 
 function App() {
   return (
-    <>
-      <h1>Formulário de Cadastro</h1>
+    <Container component="article" maxWidth="sm">
+      <Typography variant="h3" component="h1" align="center">Formulário de Cadastro</Typography>
       <FormularioCadastro />
-    </>
+    </Container>
   );
 }
 
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,29 +1,41 @@
-import { Button } from '@material-ui/core';
-import React from 'react';
+import { Button, TextField, FormControlLabel, Switch } from '@material-ui/core';
+import React, {useState} from 'react';
 
 export default function FormularioCadastro() {
+
+    const [nome, setNome] = useState("");
+    const [sobrenome, setSobrenome] = useState("");
+    const [cpf, setCPF] = useState("");
+
+    let onChangeNome = (ev) =>{
+        setNome(ev.target.value)
+    }
+
+    let onChangeSobrenome = (ev) =>{
+        setSobrenome(ev.target.value)
+    }
+
+    let onChangeCPF = (ev) =>{
+        setCPF(ev.target.value)
+    }
+
+    let onFormSubmit = (ev) => {
+        ev.preventDefault();
+
+    }
+
+
     return (
-        <form>
-            <label>Nome
-                <input type="text" />
-            </label>
-
-            <label>Sobrenome
-                <input type="text" />
-            </label>
-
-            <label>Cpf
-                <input type="text" />
-            </label>
-
-            <label>Promoções
-                <input type="checkbox" />
-            </label>
-
-            <label>Novidades
-                <input type="checkbox" />
-            </label>
-
+        <form onSubmit={onFormSubmit}>
+            <TextField margin="normal" id="noneId" label="Nome" variant="outlined" onChange={onChangeNome} fullWidth />
+            <TextField margin="normal" id="sobrenome" label="Sobrenome" variant="outlined" onChange={onChangeSobrenome} fullWidth />
+            <TextField margin="normal" id="cpfId" label="CPF" variant="outlined" onChange={onChangeCPF} fullWidth />
+            <FormControlLabel label="Promoções" control={
+                <Switch id="promocoesID" label="Promoções" defaultChecked/>
+            } />                           
+            <FormControlLabel label="Novidades" control={
+                <Switch id="novidadeID" label="Novidades" defaultChecked/>
+            } />
             <Button type="submit" variant="contained" color="primary">
                 Cadastrar
             </Button>
